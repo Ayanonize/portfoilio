33433baf196be33b594384a0ee287396ebd15601
ayan khan <ayanonize@gmail.com>
2022-07-13 11:20:50 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -366,22 +366,22 @@ export default function Home() {
           <h3 className="text-center font-header text-3xl uppercase leading-tight tracking-wide text-white sm:text-4xl lg:text-5xl">
             Keep <span className="font-bold">up-to-date</span> <br />
             with what I'm up to
-          </h3 >
-          <form className="mt-6 flex flex-col justify-center sm:flex-row" >
+          </h3>
+          <form className="mt-6 flex flex-col justify-center sm:flex-row">
             <input className="w-full rounded px-4 py-3 font-body text-black sm:w-2/5 sm:py-4 lg:w-1/3" type="text" id="email" placeholder="Give me your Email" />
-            <button className="mt-2 rounded bg-yellow-500 px-8 py-3 font-body text-base font-bold uppercase text-primary transition-colors hover:bg-[rgba(65,47,144,.93)] hover:text-white focus:border-transparent focus:outline-none focus:ring focus:ring-yellow sm:ml-2 sm:mt-0 sm:py-4 md:text-lg" >
+            <button className="mt-2 rounded bg-yellow-500 px-8 py-3 font-body text-base font-bold uppercase text-primary transition-colors hover:bg-[rgba(65,47,144,.93)] hover:text-white focus:border-transparent focus:outline-none focus:ring focus:ring-yellow sm:ml-2 sm:mt-0 sm:py-4 md:text-lg">
               Join the club
-            </button >
-          </form >
-        </div >
-      </div >
-      <div className="bg-[rgba(65,47,144,.93)]" >
-        <div className="container flex flex-col justify-between p-6 sm:flex-row" >
-          <p className="text-center font-body text-white md:text-left" >
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="bg-[rgba(65,47,144,.93)]">
+        <div className="container flex flex-col justify-between p-6 sm:flex-row">
+          <p className="text-center font-body text-white md:text-left">
             © Copyright 2022. All right reserved, AYANONIZE.
-          </p >
-        </div >
-      </div >
+          </p>
+        </div>
+      </div>
     </>
   )
 }
