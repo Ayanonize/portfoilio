9c02daa81da4b000db0e1f62a72f9d4d1b156c91
ayan khan <ayanonize@gmail.com>
2022-07-13 11:18:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -361,8 +361,27 @@ export default function Home() {
 
       </section>
 
-
-
+      <div class="relative bg-primary bg-cover bg-center bg-no-repeat py-16 bg-blend-multiply lg:py-24" style={{ backgroundImage: "url(https://atom.redpixelthemes.com/assets/img/bg-cta.jpg)" }}>
+        <div class="container relative z-30">
+          <h3 class="text-center font-header text-3xl uppercase leading-tight tracking-wide text-white sm:text-4xl lg:text-5xl">
+            Keep <span class="font-bold">up-to-date</span> <br />
+            with what I'm up to
+          </h3>
+          <form class="mt-6 flex flex-col justify-center sm:flex-row">
+            <input class="w-full rounded px-4 py-3 font-body text-black sm:w-2/5 sm:py-4 lg:w-1/3" type="text" id="email" placeholder="Give me your Email" />
+            <button class="mt-2 rounded bg-yellow-500 px-8 py-3 font-body text-base font-bold uppercase text-primary transition-colors hover:bg-[rgba(65,47,144,.93)] hover:text-white focus:border-transparent focus:outline-none focus:ring focus:ring-yellow sm:ml-2 sm:mt-0 sm:py-4 md:text-lg">
+              Join the club
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="bg-[rgba(65,47,144,.93)]">
+        <div class="container flex flex-col justify-between p-6 sm:flex-row">
+          <p class="text-center font-body text-white md:text-left">
+            © Copyright 2022. All right reserved, AYANONIZE.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
