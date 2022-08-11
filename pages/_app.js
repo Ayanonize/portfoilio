0168c3649de15441a3eb9f28a3c639a01c7bfdcb
ayan khan <99946200+Ayanonize@users.noreply.github.com>
2022-08-11 22:12:13 +0530

    favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Head>
+        <link rel="shortcut icon" href="https://pbs.twimg.com/profile_images/1481905318942867457/J0ylsqPq_400x400.jpg" type="image/x-icon" />
+      </Head>
+    </>
+  )
 }
 
 export default MyApp
