86e651a64392911860c725e1ff4fa200e4137f5d
ayan khan <99946200+Ayanonize@users.noreply.github.com>
2022-08-31 23:03:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -201,6 +201,19 @@ export default function Home() {
                   </div>
                 </div>
 
+                <div className="lg:w-1/3 sm:w-1/2 p-4">
+                  <div className="flex relative">
+                    <img alt="gallery" className="absolute inset-0 w-full h-full object-contain object-center p-5" src=" https://imgs.search.brave.com/WFKHQJOzdJxGKtGQeD5l-SaGiDGbxCP6lu9nKhnYJNg/rs:fit:900:500:1/g:ce/aHR0cHM6Ly9sb2dv/dnRvci5jb20vd3At/Y29udGVudC91cGxv/YWRzLzIwMjAvMTAv/dmVyY2VsLWluYy1s/b2dvLXZlY3Rvci5w/bmc
+" />
+
+                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-80 duration-700">
+                      <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+                      <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                      <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    </div>
+                  </div>
+                </div>
+
 
                 
                 <div className="lg:w-1/3 sm:w-1/2 p-4">
