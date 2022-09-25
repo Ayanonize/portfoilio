3bfd36f6b6ee767a30afcbf8e576e43c3498c2d2
ayan khan <99946200+Ayanonize@users.noreply.github.com>
2022-09-25 09:02:36 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -32,46 +32,46 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
-                <Script src="/assets/js/vendor/modernizr-3.5.0.min.js"></Script>
+                <Script src="/assets/js/vendor/modernizr-3.5.0.min.js" strategy='beforeInteractive'></Script>
 
-                <Script src="/assets/js/vendor/jquery-1.12.4.min.js"></Script>
-                <Script src="/assets/js/popper.min.js"></Script>
-                <Script src="/assets/js/bootstrap.min.js"></Script>
+                <Script src="/assets/js/vendor/jquery-1.12.4.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/popper.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/bootstrap.min.js" strategy='beforeInteractive'></Script>
 
-                <Script src="/assets/js/jquery.slicknav.min.js"></Script>
-
-
-                <Script src="/assets/js/owl.carousel.min.js"></Script>
-                <Script src="/assets/js/slick.min.js"></Script>
-
-                <Script src="/assets/js/wow.min.js"></Script>
-                <Script src="/assets/js/animated.headline.js"></Script>
-                <Script src="/assets/js/jquery.magnific-popup.js"></Script>
+                <Script src="/assets/js/jquery.slicknav.min.js" strategy='beforeInteractive'></Script>
 
 
-                <Script src="/assets/js/gijgo.min.js"></Script>
+                <Script src="/assets/js/owl.carousel.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/slick.min.js" strategy='beforeInteractive'></Script>
 
-                <Script src="/assets/js/jquery.nice-select.min.js"></Script>
-                <Script src="/assets/js/jquery.sticky.js"></Script>
-
-                <Script src="/assets/js/jquery.barfiller.js"></Script>
-
-
-                <Script src="/assets/js/jquery.counterup.min.js"></Script>
-                <Script src="/assets/js/waypoints.min.js"></Script>
-                <Script src="/assets/js/jquery.countdown.min.js"></Script>
-                <Script src="/assets/js/hover-direction-snake.min.js"></Script>
+                <Script src="/assets/js/wow.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/animated.headline.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/jquery.magnific-popup.js" strategy='beforeInteractive'></Script>
 
 
-                <Script src="/assets/js/contact.js"></Script>
-                <Script src="/assets/js/jquery.form.js"></Script>
-                <Script src="/assets/js/jquery.validate.min.js"></Script>
-                <Script src="/assets/js/mail-script.js"></Script>
-                <Script src="/assets/js/jquery.ajaxchimp.min.js"></Script>
+                <Script src="/assets/js/gijgo.min.js" strategy='beforeInteractive'></Script>
+
+                <Script src="/assets/js/jquery.nice-select.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/jquery.sticky.js" strategy='beforeInteractive'></Script>
+
+                <Script src="/assets/js/jquery.barfiller.js" strategy='beforeInteractive'></Script>
 
 
-                <Script src="/assets/js/plugins.js"></Script>
-                <Script src="/assets/js/main.js"></Script>
+                <Script src="/assets/js/jquery.counterup.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/waypoints.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/jquery.countdown.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/hover-direction-snake.min.js" strategy='beforeInteractive'></Script>
+
+
+                <Script src="/assets/js/contact.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/jquery.form.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/jquery.validate.min.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/mail-script.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/jquery.ajaxchimp.min.js" strategy='beforeInteractive'></Script>
+
+
+                <Script src="/assets/js/plugins.js" strategy='beforeInteractive'></Script>
+                <Script src="/assets/js/main.js" strategy='beforeInteractive'></Script>
 
 
             </body>
