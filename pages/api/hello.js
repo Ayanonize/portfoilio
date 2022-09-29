7646f0301013945e14719a9b8f33906a5f472228
ayan khan <99946200+Ayanonize@users.noreply.github.com>
2022-09-29 15:30:43 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import createConnection from "./models/createConnection"
-
-createConnection()
-
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
